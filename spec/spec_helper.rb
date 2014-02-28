1f71ca6e7641dc5c3d8fc071d541ae78b5493e67
Johnny Wu <jwu@Johnnys-MacBook-Pro.local>
2014-02-28 11:32:43 -0800

    initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+
+# All our specs should require 'spec_helper' (this file)
+
+# If RACK_ENV isn't set, set it to 'test'.  Sinatra defaults to development,
+# so we have to override that unless we want to set RACK_ENV=test from the
+# command line when we run rake spec.  That's tedious, so do it here.
+ENV['RACK_ENV'] ||= 'test'
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'shoulda-matchers'
+require 'rack/test'
+require 'factory_girl'
+require 'database_cleaner'
+
+FactoryGirl.definition_file_paths = %w{./factories ./test/factories ./spec/factories}
+FactoryGirl.find_definitions
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+end
+
+def app
+  Sinatra::Application
+end
