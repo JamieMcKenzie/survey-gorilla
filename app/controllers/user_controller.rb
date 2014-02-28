74e25e515ee79363e472172b01ee83250e2d12df
Jamie & Stephen Buchanan <pair+jamck23.papapoison@devbootcamp.com>
2014-02-28 15:57:17 -0800

    Added user routes and tests.

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,26 @@
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  auth_user(params)
+
+  if logged_in?
+    redirect "/users/#{session[:id]}"
+  else
+    redirect '/login'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :show_user
+end
+
+post '/users' do
+  @user = User.create(params)
+  session[:id] = @user.id
+  redirect "/users/#{@user.id}"
+end
+
+
