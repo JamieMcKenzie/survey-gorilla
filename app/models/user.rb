65b6d9d2e559cfaaf67a0c3fbceff99534c52170
Johnny Wu <jonathan.x.wu@gmail.com>
2014-02-28 12:31:03 -0800

    woo

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name, :email, :password
+  validates_uniqueness_of :name, :email
+  validates_format_of :email, with: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/
+  validates_format_of :password, with: /^(?=.*\d).{4,10}$/
+
+  has_many :forms
+  has_many :entries
+
+  before_save :encrypt
+
+  def encrypt
+    self.password = BCrypt::Password.create(self.password)
+  end
+
+  def correct_password?(string)
+    hashed_password = BCrypt::Password.new(self.password)
+    hashed_password == string
+  end
+end
