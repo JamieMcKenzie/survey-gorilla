84e63070fc6342651d8e4d7900ba113ceb53fbcf
Apprentice <apprentice@dbc16.local>
2014-02-28 11:14:57 -0800

    rename bad file name and table name :

--- capuchins/db/migrate/20140228105339_create_questions.rb
+++ capuchins/db/migrate/20140228105339_create_questions.rb
@@ -1,6 +1,6 @@
 class CreateQuestion < ActiveRecord::Migration
   def change
-    create_table :question do |t|
+    create_table :questions do |t|
       t.belongs_to :survey
       t.text       :text, null: false
       t.timestamps
