8308f286b4309677f693268c81e9df59d8e91ba3
Johnny Wu <jonathan.x.wu@gmail.com>
2014-02-28 15:58:51 -0800

    updates migration

--- db/migrate/20140228114811_create_entries.rb
+++ db/migrate/20140228114811_create_entries.rb
@@ -3,6 +3,7 @@ class CreateEntries < ActiveRecord::Migration
     create_table :entries do |t|
       t.belongs_to :user
       t.belongs_to :option
+      t.belongs_to :form
 
       t.timestamps
     end
