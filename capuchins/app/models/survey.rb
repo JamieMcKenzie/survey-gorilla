c130c0d8a3983c1088d97bfdc05889d131dedf0a
Apprentice <apprentice@dbc16.local>
2014-02-28 12:10:17 -0800

    Added fill_survey view Added fill_survey controller Get fill survey to work Get fill survey URL to use random sha rather than id Changed Survey model and filled in generate_token function Fixed create_questions migrate file Updated things in seed file

--- capuchins/app/models/survey.rb
+++ capuchins/app/models/survey.rb
@@ -4,5 +4,9 @@ class Survey < ActiveRecord::Base
   validates_presence_of :title
 
   def generate_token
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless Survey.exists?(token: random_token)
+    end
   end
 end
