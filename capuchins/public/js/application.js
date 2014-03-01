1e194ea62d109b19dc2a5550aa162c06155ba7a1
Anne Willborn & Harry Whelchel <pair+awillborn.harryw377@devbootcamp.com>
2014-02-28 16:43:15 -0800

    Refactor Javascript Soup.

--- capuchins/public/js/application.js
+++ capuchins/public/js/application.js
@@ -1,26 +1,34 @@
+var $Wrapper = {
+
+  addQuestion: function(){
+    $('.add_question').click(function(e){
+          var newQuestion = '<label for="question">Question:</label><input type="text" name="questions[]" id="question" class="question">'
+          $(newQuestion).insertAfter('.add_question');
+        });
+  },
+  submitForm: function(){
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "/surveys",
+        data: $('form').serialize()
+      }).success(function(resp){
+        $('form').css("display","none");
+        $('.url_display').append("Survey Link: <a href='/surveys/"+resp+"'>/surveys/"+resp + "</a>");
+      }).fail(function(resp){
+        $('form').css("display","none");
+        $('.url_display').append("Error. No URL!");
+      });
+    });
+  }
+}
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
-  $('.add_question').click(function(e){
-        var newQuestion = '<label for="question">Question:</label><input type="text" name="questions[]" id="question" class="question">'
-        $(newQuestion).insertAfter('.add_question');
-      });
-
-  $('form').submit(function(e){
-    e.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "/surveys",
-      data: $('form').serialize()
-    }).success(function(resp){
-      $('form').css("display","none");
-      $('.url_display').append("Survey Link: <a href='/surveys/"+resp+"'>/surveys/"+resp + "</a>");
-    }).fail(function(resp){
-      $('form').css("display","none");
-      $('.url_display').append("Error. No URL!");
-    });
-  });
-
+  $Wrapper.addQuestion();
+  $Wrapper.submitForm();
 });
+
