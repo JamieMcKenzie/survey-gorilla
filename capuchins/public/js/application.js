b2ecfbb0a21a04d52c9ddd3b8499ef9f9eefc26c
Anne Willborn & Harry Whelchel <pair+awillborn.harryw377@devbootcamp.com>
2014-02-28 17:02:30 -0800

    fd

--- capuchins/public/js/application.js
+++ capuchins/public/js/application.js
@@ -1,5 +1,26 @@
 var $Wrapper = {
 
+  answerListener: function(){
+    $("form .answer_form").submit (function(e){
+      e.preventDefault();
+      postAnswer();
+    });
+  },
+
+  postAnswer: function(){
+      var path = $(".answer_form").attr("action"); //later see if can take out
+      var data = $("form").serialize();
+      $.ajax({
+        type: "POST",
+        url: path,
+        data: data
+      }).success(function(msg){
+        $(".answer_form").prepend("<h1>" + msg + "</h1>");
+      }).fail(function(){
+        $(".header").append("<h1>Submission Failed! Try again!</h1>");
+      });
+  },
+
   addQuestion: function(){
     $('.add_question').click(function(e){
           var newQuestion = '<label for="question">Question:</label><input type="text" name="questions[]" id="question" class="question">'
@@ -7,7 +28,7 @@ var $Wrapper = {
         });
   },
   submitForm: function(){
-    $('form').submit(function(e){
+    $('form .survey').submit(function(e){
       e.preventDefault();
       $.ajax({
         type: "POST",
@@ -23,11 +44,12 @@ var $Wrapper = {
     });
   }
 }
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
-
+  $Wrapper.answerListener();
   $Wrapper.addQuestion();
   $Wrapper.submitForm();
 });
