a8c4d5c57395386c2577102e8df6cd5df24c2893
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-03 00:42:00 -0800

    Add skeleton for tests.

--- capuchins/spec/integrations/survey_controller_spec.rb
+++ capuchins/spec/integrations/survey_controller_spec.rb
@@ -2,20 +2,30 @@ require 'spec_helper'
 
 describe 'Survey Controller' do
 
-  context 'make survey page' do
+  describe 'make survey page' do
     it 'should exist' do
       get '/surveys/new'
 
       expect(last_response).to be_ok
-
     end
 
     it 'should include "Create Survey"' do
       get '/surveys/new'
 
       expect(last_response.body).to include("Create Survey")
-
     end
+  end
+
+  describe 'post request to add question' do
+  end
+
+  describe 'post request to complete survey submission' do
+  end
+
+  describe 'generate URL token for survey' do
+  end
+
+  describe 'post to record users answers to survey' do
   end
 
 end
---
+++ capuchins/spec/integrations/user_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'User Controller' do
+
+  describe 'login' do
+  end
+
+  describe 'create user' do
+  end
+
+  describe 'user profile page' do
+  end
+
+  describe 'logout' do
+  end
+
+end
