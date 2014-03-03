9f919d4b323cd1035d627610e7cfc8271f71c012
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-02 20:57:30 -0800

    Updates name of answer table to users_choices

--- capuchins/db/migrate/20140228105349_create_users_choices.rb
+++ capuchins/db/migrate/20140228105349_create_users_choices.rb
@@ -1,6 +1,6 @@
-class CreateAnswers < ActiveRecord::Migration
+class CreateUsersChoices < ActiveRecord::Migration
   def change
-    create_table :answers do |t|
+    create_table :users_choices do |t|
       t.belongs_to :choice
       t.belongs_to :user
       t.timestamps
