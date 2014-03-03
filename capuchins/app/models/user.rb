0bc8cda2296dd7abdbf7d27e2ccf986e01829616
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-02 21:59:20 -0800

    Adds BCrypt support for user passwords

--- capuchins/app/models/user.rb
+++ capuchins/app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   has_many :choices
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
--- capuchins/db/migrate/20140301132447_create_users_table.rb
+++ capuchins/db/migrate/20140301132447_create_users_table.rb
@@ -2,7 +2,7 @@ class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, null: false
-      t.string :password, null: false
+      t.string :password_hash, null: false
       t.timestamps
     end
   end
